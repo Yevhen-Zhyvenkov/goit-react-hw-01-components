5d282ea314489c28ac22f9a01123deb7c1f4221c
Yevhen-Zhyvenkov <zheka123730@gmail.com>
2023-06-07 17:10:54 +0300

    fix

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,9 +1,9 @@
 import css from './Statistics.module.css'
 import PropTypes from 'prop-types';
 
-const Statistics = ({ title, stats }) => {
+const Statistics = ({title,  stats }) => {
     return <section className={css.statistics}>
-        <h2 className={css.title}>{title}</h2>
+        {title && <h2 className={css.title}>{title}</h2>}
 
         <ul className={css['stat__list']}>{stats.map(({ id, label, percentage }) => {
             return (<li className={css.item} key={id}>
